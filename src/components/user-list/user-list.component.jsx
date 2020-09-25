87200f49995b8c182b4a1b7d5a5f46bb23d9f6cd
NestorMonroy <nestor.monroy.90@gmail.com>
2020-09-25 17:13:35 -0500

    first commit

---
+++ src/components/user-list/user-list.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class UserList extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state ={
+            users: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then( data => this.setState({ users: data.slice(0,3) }))
+    }
+    
+
+    render(){
+        return (
+            <div className='container user-list'>
+                <h1> Users list </h1>
+                {
+                    this.state.users.map(user => 
+                    <div className='post' key={user.id} > 
+                        <h1> {user.name} </h1>
+                        <h2> {user.email} </h2>
+                    </div> )
+                }
+            </div>
+        )
+    }
+}
+export default UserList;
